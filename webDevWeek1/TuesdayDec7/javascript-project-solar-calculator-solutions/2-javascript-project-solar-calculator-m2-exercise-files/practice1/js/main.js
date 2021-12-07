4e9f1f81eb61d0f07204668e35856ffae41c0a69
Daravy DeRouen <daravyderouen@gmail.com>
2021-12-07 16:34:04 -0600

    Revised document

--- webDevWeek1/TuesdayDec7/javascript-project-solar-calculator-solutions/2-javascript-project-solar-calculator-m2-exercise-files/practice1/js/main.js
+++ webDevWeek1/TuesdayDec7/javascript-project-solar-calculator-solutions/2-javascript-project-solar-calculator-m2-exercise-files/practice1/js/main.js
@@ -1,1 +1,7 @@
 // JavaScript Document
+//step 4, before this refer back to Word document 
+//alert('Hello World from Me')//gets an alert in browser
+//JavaScript is executed from top to bottom
+
+
+
