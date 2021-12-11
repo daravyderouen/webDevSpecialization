33e6fd3f98327619df460b9d97f8e9875358c31b
Daravy DeRouen <daravyderouen@gmail.com>
2021-12-11 01:08:25 -0600

    Adding files for Friday

---
+++ webDevWeek1/fridayDec10/javaScriptUsingWhileAndForLoopsInteractive.js
@@ -0,0 +1,20 @@
+//THE LABYRINTH OF LOOPS
+//WHILE LOOPS
+//The while-loop runs its code as long as its boolean expression is true
+//basic while loop setup
+while(/*some expression is true */){
+    /*do this code!*/
+}
+
+
+
+//CHALLENGE: a BASIC WHILE LOOP
+
+
+
+
+//CHALLENGE: PROBLEM SOLVING WITH LOOP OPERATIONS
+
+
+
+//MORE LOOPS
