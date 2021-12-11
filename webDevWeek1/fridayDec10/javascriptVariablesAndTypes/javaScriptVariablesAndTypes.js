d8c7fcbb83f10f44e71a4ee9bd6d4cb7599bd73e
Daravy DeRouen <daravyderouen@gmail.com>
2021-12-11 00:38:17 -0600

    Adding in Friday's Folder

---
+++ webDevWeek1/fridayDec10/javascriptVariablesAndTypes/javaScriptVariablesAndTypes.js
@@ -0,0 +1,30 @@
+//Course Overview
+
+//**USING VARIABLES, LITERALS & ASSIGNMENTS**/  
+
+//How this Course Works
+//DEMO, DEMOS, DEMOS
+//Basic knowledge of VS Code and JavaScript web Development
+//You can easily clone a project by View>>Command palette>>Git clone command
+//Cloned the folder
+
+//Using Template Literals
+
+
+//Create a Tagged Templace Literal
+
+
+//The Difference Between Let and Const
+
+
+//Use the Destructuring Syntax to Get Values From Arrays & Objects
+
+//Summary and Where to Go Next
+
+
+
+
+
+
+
+//**APPLYING PRIMITIVE TYPES**/
